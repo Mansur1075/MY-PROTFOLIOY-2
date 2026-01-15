2719a68bd5791587b792f65826e0845ce6ba669f
Mansur <hmmansur050@gmail.com>
2026-01-15 20:41:37 +0600

    porotfolio part

--- src/componntes/Portfolio.jsx
+++ src/componntes/Portfolio.jsx
@@ -1,9 +1,23 @@
 import React from 'react'
+import Container from './Container'
 
 const Portfolio = () => {
   return (
-   <section>
-    <h4>MANSUR</h4>
+   <section className='bg-linear-to-r  from-slate-900  to-slate-900 py-10'>
+    <Container>
+        <div className="">
+            <div className=" text-center">
+                <h3 className='text-[25px] uppercase text-[#00D3F3] font-lato '>Portfolio</h3>
+                <h2 className='text-[50px] text-[#ffffffe4] font-bold font-lato'>Featured <span className='text-[50px] text-[#00D3F3] font-bold font-lato'>Projects</span></h2>
+            </div>
+            <div className="">
+                <div className=" w-6/12">
+                <img src="" alt="" />
+                </div>
+                <div className=" w-6/12"></div>
+            </div>
+        </div>
+    </Container>
    </section>
   )
 }
