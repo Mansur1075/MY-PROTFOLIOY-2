b86f6d0a17e3cf69b84a9341f301a3459d811505
Mansur1075 <hmmansur050@gmail.com>
2026-02-23 10:16:35 -0800

    mansur

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,12 @@
 import { useState } from 'react'
 import './App.css'
-import Container from './componntes/Container'
 import { Navbar } from './componntes/Navbar'
 import Bennar from './componntes/Bennar'
 import About from './componntes/About'
 import Skills from './componntes/Skills'
 import Portfolio from './componntes/Portfolio'
 import Footer from './componntes/Footer'
+import Contract from './componntes/Contract'
 
 
 function App() {
@@ -20,6 +20,7 @@ function App() {
     <About/>
     <Skills/>
     <Portfolio/>
+    <Contract/>
     <Footer/>
    </div>
     </>
